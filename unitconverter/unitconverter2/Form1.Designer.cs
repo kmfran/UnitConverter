8be7e1e48ce28fdd870981bd6c0fbe7c0c4245cc
kmfran <73110907+kmfran@users.noreply.github.com>
2020-12-07 20:11:59 +0100

    Add files via upload

---
+++ unitconverter/unitconverter2/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace unitconverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.UserInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.UserOutput = new System.Windows.Forms.TextBox();
+            this.Liczba = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.unitsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.weightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // UserInput
+            // 
+            this.UserInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UserInput.Location = new System.Drawing.Point(95, 35);
+            this.UserInput.Name = "UserInput";
+            this.UserInput.Size = new System.Drawing.Size(153, 20);
+            this.UserInput.TabIndex = 2;
+            this.UserInput.TextChanged += new System.EventHandler(this.UserInput_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 71);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 69);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(204, 71);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 69);
+            this.listBox2.TabIndex = 4;
+            // 
+            // UserOutput
+            // 
+            this.UserOutput.Enabled = false;
+            this.UserOutput.Location = new System.Drawing.Point(95, 221);
+            this.UserOutput.Name = "UserOutput";
+            this.UserOutput.Size = new System.Drawing.Size(153, 20);
+            this.UserOutput.TabIndex = 5;
+            this.UserOutput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Liczba
+            // 
+            this.Liczba.AutoSize = true;
+            this.Liczba.Location = new System.Drawing.Point(34, 38);
+            this.Liczba.Name = "Liczba";
+            this.Liczba.Size = new System.Drawing.Size(41, 13);
+            this.Liczba.TabIndex = 6;
+            this.Liczba.Text = "Liczba:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Wynik:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.unitsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(336, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // unitsToolStripMenuItem
+            // 
+            this.unitsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.weightToolStripMenuItem});
+            this.unitsToolStripMenuItem.Name = "unitsToolStripMenuItem";
+            this.unitsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.unitsToolStripMenuItem.Text = "Units";
+            this.unitsToolStripMenuItem.Click += new System.EventHandler(this.unitsToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(112, 22);
+            this.toolStripMenuItem2.Text = "Lenght";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // weightToolStripMenuItem
+            // 
+            this.weightToolStripMenuItem.Name = "weightToolStripMenuItem";
+            this.weightToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.weightToolStripMenuItem.Text = "Weight";
+            this.weightToolStripMenuItem.Click += new System.EventHandler(this.weightToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 276);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Liczba);
+            this.Controls.Add(this.UserOutput);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.UserInput);
+            this.Controls.Add(this.button1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Unit Converter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox UserInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox UserOutput;
+        private System.Windows.Forms.Label Liczba;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem unitsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem weightToolStripMenuItem;
+    }
+}
+
---
+++ unitconverter/unitconverter2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace unitconverter
+{
+    public partial class Form1 : Form
+    {
+        public static List<UnitType> Lenght = new List<UnitType>()
+            {
+                new UnitType() {Value=1m, Name="Meter"},
+                new UnitType() {Value=0.000621371192m, Name="Mile"},
+                new UnitType() {Value=3.2808399m, Name="Foot"},
+                new UnitType() {Value=39.3700787m, Name="Inch"},
+                new UnitType() {Value=0.001m, Name="Kilometer"},
+                new UnitType() {Value=100m, Name="Centimeter"}
+            };
+
+        public static List<UnitType> Weight = new List<UnitType>()
+        {
+                new UnitType() {Value=1m, Name="Kilogram"},
+                new UnitType() {Value=35.2739619m, Name="Ounce"},
+                new UnitType() {Value=0.001m, Name="Ton"},
+                new UnitType() {Value=2.20462262m, Name="Pound"}
+        };
+
+        List<List<UnitType>> Units = new List<List<UnitType>>();
+
+        List<decimal> UsedUnits = new List<decimal>();
+        
+
+        public Form1()  
+        {
+            InitializeComponent();
+            unitsToolStripMenuItem.Text = nameof(Units);
+            Units.Add(Lenght);
+            Units.Add(Weight);
+            
+            for (int i = 0; i < Lenght.Count; i++)
+            {
+                listBox1.Items.Add(Lenght[i].Name);
+                listBox2.Items.Add(Lenght[i].Name);
+                UsedUnits.Add(Lenght[i].Value);
+            }
+            
+            
+        }
+        private void UserInput_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if ((listBox1.SelectedIndex == -1) || (listBox2.SelectedIndex == -1))
+            {
+                MessageBox.Show("Please select an Item first!");
+            }
+            else
+            {               
+                decimal x;
+                if (!(decimal.TryParse(UserInput.Text, out x)))
+                {
+                    MessageBox.Show("Błąd!");
+                }
+                else
+                {
+                    decimal wynik = (x * ((UsedUnits[0] / UsedUnits[listBox1.SelectedIndex]) / (UsedUnits[0] / UsedUnits[listBox2.SelectedIndex])));
+                    string wynik_s = wynik.ToString();
+
+                    UserOutput.Text = wynik_s;
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void unitsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                    
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+           for(int j=listBox1.Items.Count-1;j>=0;--j)
+            {
+                listBox1.Items.RemoveAt(j);
+                listBox2.Items.RemoveAt(j);
+                UsedUnits.RemoveAt(j);
+            }
+
+            for (int i = 0; i < Lenght.Count; i++)
+            {
+                listBox1.Items.Add(Lenght[i].Name);
+                listBox2.Items.Add(Lenght[i].Name);
+                UsedUnits.Add(Lenght[i].Value);
+            }
+        }
+
+        private void weightToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            for (int j = listBox1.Items.Count - 1; j >= 0; --j)
+            {
+                listBox1.Items.RemoveAt(j);
+                listBox2.Items.RemoveAt(j);
+                UsedUnits.RemoveAt(j);
+            }
+
+            for (int i = 0; i < Weight.Count; i++)
+            {
+                listBox1.Items.Add(Weight[i].Name);
+                listBox2.Items.Add(Weight[i].Name);
+                UsedUnits.Add(Weight[i].Value);
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
